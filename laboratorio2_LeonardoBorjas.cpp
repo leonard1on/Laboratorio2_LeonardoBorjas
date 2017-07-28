4e8b8dd0759148198b3b0da0835e72514b6c7797
Leonardo <leonardborj@gmail.com>
2017-07-28 16:08:53 -0600

    Corregir

--- laboratorio2_LeonardoBorjas.cpp
+++ laboratorio2_LeonardoBorjas.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 int menu();
 float ejercicio1();
-float ejercicio2sen(float);
-float ejercicio2cos(float);
+double ejercicio2sen(double);
+double ejercicio2cos(double);
 int factorial(int);
 int main(){
 	int resp=0;
@@ -17,11 +17,12 @@ int main(){
 				   }
 					break;
 			case 2:{
-						float angulo;
+						double angulo;
 						cout<<"Ingrese un angulo para sacar la serie de coseno y seno:";
 						cin >>angulo;
 						angulo=(3.141592*angulo)/180;
-
+						cout <<"El coseno es " <<ejercicio2cos(angulo) <<endl;
+						cout <<"El seno es " <<ejercicio2sen(angulo) <<endl;
 				   
 				   }
 					break;
@@ -71,27 +72,27 @@ float ejercicio1(){
 	return distancia;
 }
 
-float ejercicio2sen(float angulo){
-	float sensuma;
+double ejercicio2sen(double angulo){
+	double sensuma=0;
 
-	for(int n=0;n<12;n++){
+	for(int n=0;n<5;n++){
 		int factorialsin=factorial(2*n+1);
-		float parte1sin= (pow (-1,n))/(factorialsin);
-		float parte2sin= pow (angulo,2*n+1);
-		float seno=parte1sin*parte2sin;
+		double parte1sin= (pow (-1,n));
+		double parte2sin= pow (angulo,(2*n+1));
+		double seno=(parte1sin*parte2sin)/factorialsin;
 		sensuma+=seno;
 	}
 	return sensuma;
 }
 
-float ejercicio2cos(float angulo){
-	float cossuma;
+double ejercicio2cos(double angulo){
+	double cossuma;
 	
-	for(int n=0;n<12;n++){
-		int factorialcos=factorial(2*n);
-		float parte1cos= (pow (-1,n))/(factorialcos);
-		float parte2cos= pow (angulo,2*n);
-		float cos=parte1cos*parte2cos;
+	for(int n=0;n<5;n++){
+		long factorialcos=factorial(2*n);
+		double parte1cos= (pow (-1,n));
+		double parte2cos= pow (angulo,(2*n));
+		double cos=(parte1cos*parte2cos)/factorialcos;
 		cossuma+=cos;
 	}
 	return cossuma;
@@ -99,7 +100,7 @@ float ejercicio2cos(float angulo){
 
 
 int factorial(int limite){
-	int resp=1;
+	long resp=1;
 	for(int i=1;i<=limite;i++){
 		resp*=i;
 	}
